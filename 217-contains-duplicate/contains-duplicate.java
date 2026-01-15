5d6cc958bb0276d152f1e4710748336f63f7cc56
Kalyan <129888587+k-a-l@users.noreply.github.com>
2026-01-15 15:45:48 +0545

    Time: 15 ms (64.25%) | Memory: 93.5 MB (43.09%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -9,13 +9,23 @@ class Solution {
     //     return false;
     // }
 
-    Map<Integer, Integer> map = new HashMap<>();
-    for(int i = 0; i < nums.length; i++){
-        if(map.containsKey(nums[i])){
+    // Map<Integer, Integer> map = new HashMap<>(); //O(n)
+    // for(int i = 0; i < nums.length; i++){ //O(n)
+    //     if(map.containsKey(nums[i])){
+    //         return true;
+    //     }
+    //     map.put(nums[i], i);
+    // }
+    // return false;
+    // }
+
+    Set<Integer> set = new HashSet<>();
+    for(int num : nums){
+        if(set.contains(num)){
             return true;
         }
-        map.put(nums[i], i);
+        set.add(num);
     }
     return false;
-    }
+}
 }
