fe16ceb7ed29ac837d440aa88a1c06597173f10b
Kalyan <129888587+k-a-l@users.noreply.github.com>
2026-01-18 19:35:43 +0545

    Time: 0 ms (100.00%) | Memory: 43.9 MB (25.10%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -16,18 +16,31 @@ class Solution {
     //Simple brute force
     int min = nums[0];
     int index = 0;
-    for(int i = 0; i < nums.length; i++){
-        // if(min > nums[i]){
-        //     min = nums[i];
-        // }
-        // min = min > num ? num : min;
-        min = Math.min(min, nums[i]);
-        index = i;
+    // for(int i = 0; i < nums.length; i++){
+    //     // if(min > nums[i]){
+    //     //     min = nums[i];
+    //     // }
+    //     // min = min > num ? num : min;
+    //     min = Math.min(min, nums[i]);
+    //     index = i;
 
 
-    }
-    System.out.println(index);
-    return min;
+    // }
+    // System.out.println(index);
+    // return min;
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left < right){
+            int mid = left + (right - left) / 2; //compute same as right - left but safely 2left+right−left​=2left+right
+            if(nums[mid] < nums[right]){
+                right = mid;
+            }else{
+                left = mid + 1;
+            }
+        }
+    return nums[left];
   }
     
 }
