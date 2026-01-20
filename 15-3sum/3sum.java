9f1becb27beb1187a57c43d5484235746e3c9bf5
Kalyan <129888587+k-a-l@users.noreply.github.com>
2026-01-20 13:47:57 +0545

    Time: 27 ms (98.18%) | Memory: 60.4 MB (9.07%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -20,24 +20,49 @@ class Solution {
     //     Set<List<Integer>> allResultTriplets = new HashSet<>(result);
     //     return new ArrayList<>(allResultTriplets);
     // }
-    Set<List<Integer>> unique = new HashSet<>();
+    // Set<List<Integer>> unique = new HashSet<>();
 
-        for (int i = 0; i < nums.length - 2; i++) {
-            Set<Integer> seen = new HashSet<>();
+    //     for (int i = 0; i < nums.length - 2; i++) {
+    //         Set<Integer> seen = new HashSet<>();
 
-            for (int j = i + 1; j < nums.length; j++) {
-                int third = -nums[i] - nums[j];
+    //         for (int j = i + 1; j < nums.length; j++) {
+    //             int third = -nums[i] - nums[j];
 
-                if (seen.contains(third)) {
-                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], third);
-                    Collections.sort(triplet);
-                    unique.add(triplet);
+    //             if (seen.contains(third)) {
+    //                 List<Integer> triplet = Arrays.asList(nums[i], nums[j], third);
+    //                 Collections.sort(triplet);
+    //                 unique.add(triplet);
+    //             }
+
+    //             seen.add(nums[j]);
+    //         }
+    //     }
+
+    //     return new ArrayList<>(unique);
+    // }
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        int i,j,k;
+        for(i=0;i<nums.length;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            j=i+1; k=nums.length-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum > 0) k--;
+                else if(sum < 0) j++;
+                else{
+                    List<Integer> l = new ArrayList<>();
+                    l.add(nums[i]);
+                    l.add(nums[j]);
+                    l.add(nums[k]);
+                    res.add(l);
+                    k--;
+                    j++;
+
+                    while(j<k && nums[j]==nums[j-1]) j++;
                 }
-
-                seen.add(nums[j]);
             }
         }
-
-        return new ArrayList<>(unique);
+        return res;
     }
 }
